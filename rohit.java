2b4ce44d0008007af2527c4ff732c8593ffb81d2
rohitkumarpandey9 <146954966+rohitkumarpandey9@users.noreply.github.com>
2024-01-17 00:57:53 +0530

    Add files via upload

---
+++ rohit.java
@@ -0,0 +1,10 @@
+public class rohit{
+    public static void main(String[]args){
+        System.out.println("*\n**\n***");
+        int a=2;
+        int b=4;
+        int sum=a+b;
+        System.out.println(sum);
+       
+    }
+}
